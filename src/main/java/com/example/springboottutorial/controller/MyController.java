ee1c18a57bdedd50c293164e5936351d33a79fb7
Antonino Cacicia <antonino.cacicia@demetrix.it>
2024-02-20 11:17:59 +0100

    Add post method

--- src/main/java/com/example/springboottutorial/controller/MyController.java
+++ src/main/java/com/example/springboottutorial/controller/MyController.java
@@ -1,19 +1,21 @@
 package com.example.springboottutorial.controller;
 
 import com.example.springboottutorial.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class MyController {
-
     // GetMapping("greeting")
     @RequestMapping(method = GET, path = "greeting")
     public Greeting getGreeting() {
         return new Greeting(1, "hello");
+    }
+
+    @RequestMapping(method = POST, path = "greeting", consumes = "application/json")
+    public Greeting postGreeting (@RequestBody Greeting greeting) {
+        return greeting;
     }
 }
